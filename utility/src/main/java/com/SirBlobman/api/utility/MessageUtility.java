099c9b3eced1caaa6ba63d48538a97e22e730318
SirBlobman <blobmansrealm@gmail.com>
2020-08-25 21:02:10 -0400

    2.0-SNAPSHOT: Finished Utility Module

---
+++ utility/src/main/java/com/SirBlobman/api/utility/MessageUtility.java
@@ -0,0 +1,59 @@
+package com.SirBlobman.api.utility;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.bukkit.ChatColor;
+
+public final class MessageUtility {
+    /**
+     * @param message The message that will be colored
+     * @return A new string containing {@code message} but with the color codes replaced, or an empty string if message was {@code null}.
+     */
+    public static String color(String message) {
+        if(message == null) return "";
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+
+    /**
+     * @param messageArray The array of messages that will be colored
+     * @return A new array containing every message in the input array, but with color codes replaced.
+     */
+    public static String[] colorArray(String... messageArray) {
+        int messageArrayLength = messageArray.length;
+        String[] colorMessageArray = new String[messageArrayLength];
+
+        for(int i = 0; i < messageArrayLength; i++) {
+            String message = messageArray[i];
+            colorMessageArray[i] = color(message);
+        }
+
+        return colorMessageArray;
+    }
+
+
+    /**
+     * @param messageList The iterable of messages that will be colored
+     * @return A {@code List<String>} containing every message in the input iterable, but with color codes replaced.
+     * @see List
+     * @see java.util.Collection
+     */
+    public static List<String> colorList(Iterable<String> messageList) {
+        List<String> colorList = new ArrayList<>();
+        for(String message : messageList) {
+            String color = color(message);
+            colorList.add(color);
+        }
+        return colorList;
+    }
+
+    /**
+     * @param messageArray The array of messages that will be colored
+     * @return A {@code List<String>} containing every message in the input array, but with color codes replaced.
+     */
+    public static List<String> colorList(String... messageArray) {
+        List<String> messageList = Arrays.asList(messageArray);
+        return colorList(messageList);
+    }
+}
---
+++ utility/src/main/java/com/SirBlobman/api/utility/VersionUtility.java
@@ -0,0 +1,68 @@
+package com.SirBlobman.api.utility;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+
+public final class VersionUtility {
+    /** Pattern that matches how most server jars format the current version */
+    private static final Pattern VERSION_PATTERN = Pattern.compile("(\\(MC: )([\\d.]+)(\\))");
+
+    /**
+     * @return The current Minecraft version of the server (Example: 1.16.2)
+     */
+    public static String getMinecraftVersion() {
+        String bukkitVersion = Bukkit.getBukkitVersion();
+        Matcher matcher = VERSION_PATTERN.matcher(bukkitVersion);
+        return (matcher.find() ? matcher.group(2) : "");
+    }
+
+    /**
+     * @return The current NMS version of the server (Example: 1_16_R2)
+     */
+    public static String getNetMinecraftServerVersion() {
+        Server server = Bukkit.getServer();
+        Class<? extends Server> serverClass = server.getClass();
+        Package serverPackage = serverClass.getPackage();
+        String serverPackageName = serverPackage.getName();
+
+        int lastPeriodIndex = serverPackageName.lastIndexOf('.');
+        int nextIndex = (lastPeriodIndex + 2);
+        return serverPackageName.substring(nextIndex);
+    }
+
+    /**
+     * @return The current major.minor version of the server (Example: 1.16)
+     */
+    public static String getMajorMinorVersion() {
+        String minecraftVersion = getMinecraftVersion();
+        int lastPeriodIndex = minecraftVersion.lastIndexOf('.');
+        return (lastPeriodIndex < 2 ? minecraftVersion : minecraftVersion.substring(0, lastPeriodIndex));
+    }
+
+    /**
+     * @return The current major version of the server as an integer (Example: {@code 1})
+     */
+    public static int getMajorVersion() {
+        String majorMinorVersion = getMajorMinorVersion();
+        int periodIndex = majorMinorVersion.indexOf('.');
+
+        String majorString = majorMinorVersion.substring(0, periodIndex);
+        return Integer.parseInt(majorString);
+    }
+
+
+    /**
+     * @return The current minor version of the server as an integer (Example: {@code 16})
+     */
+    public static int getMinorVersion() {
+        String majorMinorVersion = getMajorMinorVersion();
+        int periodIndex = majorMinorVersion.indexOf('.');
+        int nextIndex = (periodIndex + 1);
+
+        String minorString = majorMinorVersion.substring(nextIndex);
+        return Integer.parseInt(minorString);
+    }
+}
