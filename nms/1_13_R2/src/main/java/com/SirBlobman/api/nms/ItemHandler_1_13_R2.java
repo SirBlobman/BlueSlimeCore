67ec3485eec88f353999160a187b4b8ebd9ea0ca
SirBlobman <blobmansrealm@gmail.com>
2020-08-27 01:22:29 -0400

    2.0-SNAPSHOT: Completed the NMS Module
    - 1_12_R1 (Spigot 1.12.2)
    - 1_13_R2 (Spigot 1.13.2)
    - 1_14_R1 (Spigot 1.14.4)
    - 1_15_R1 (Spigot 1.15.2)
    - 1_16_R2 (Spigot 1.16.2)

---
+++ nms/1_13_R2/src/main/java/com/SirBlobman/api/nms/ItemHandler_1_13_R2.java
@@ -0,0 +1,111 @@
+package com.SirBlobman.api.nms;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import org.bukkit.craftbukkit.v1_13_R2.inventory.CraftItemStack;
+import net.minecraft.server.v1_13_R2.MojangsonParser;
+import net.minecraft.server.v1_13_R2.NBTTagCompound;
+
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+
+public class ItemHandler_1_13_R2 extends ItemHandler {
+    public ItemHandler_1_13_R2(JavaPlugin plugin) {
+        super(plugin);
+    }
+
+    @Override
+    public String getLocalizedName(ItemStack item) {
+        if(item == null) return "Air";
+
+        ItemMeta meta = item.getItemMeta();
+        if(meta != null && meta.hasDisplayName()) return meta.getDisplayName();
+
+        net.minecraft.server.v1_13_R2.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
+        return nmsItem.getName().getText();
+    }
+
+    @Override
+    public String toNBT(ItemStack item) {
+        NBTTagCompound nbtData = new NBTTagCompound();
+        net.minecraft.server.v1_13_R2.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
+
+        nmsItem.save(nbtData);
+        return nbtData.toString();
+    }
+
+    @Override
+    public ItemStack fromNBT(String string) {
+        try {
+            NBTTagCompound nbtData = MojangsonParser.parse(string);
+            net.minecraft.server.v1_13_R2.ItemStack nmsItem = net.minecraft.server.v1_13_R2.ItemStack.a(nbtData);
+            return CraftItemStack.asBukkitCopy(nmsItem);
+        } catch(CommandSyntaxException ex) {
+            JavaPlugin plugin = getPlugin();
+            Logger logger = plugin.getLogger();
+            logger.log(Level.WARNING, "Failed to parse an NBT string to an item, returning AIR...", ex);
+            return new ItemStack(Material.AIR);
+        }
+    }
+
+    @Override
+    public ItemStack setCustomNBT(ItemStack item, String key, String value) {
+        if(item == null || key == null || key.isEmpty() || value == null) return item;
+
+        net.minecraft.server.v1_13_R2.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
+        NBTTagCompound nbtData = nmsItem.hasTag() ? nmsItem.getTag() : new NBTTagCompound();
+        if(nbtData == null) nbtData = new NBTTagCompound();
+
+        JavaPlugin plugin = getPlugin();
+        String pluginName = plugin.getName();
+
+        NBTTagCompound customData = nbtData.getCompound(pluginName);
+        customData.setString(key, value);
+        nbtData.set(pluginName, customData);
+
+        nmsItem.setTag(nbtData);
+        return CraftItemStack.asBukkitCopy(nmsItem);
+    }
+
+    @Override
+    public String getCustomNBT(ItemStack item, String key, String defaultValue) {
+        if(item == null || key == null || key.isEmpty()) return defaultValue;
+
+        net.minecraft.server.v1_13_R2.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
+        NBTTagCompound nbtData = nmsItem.hasTag() ? nmsItem.getTag() : new NBTTagCompound();
+        if(nbtData == null) nbtData = new NBTTagCompound();
+
+        JavaPlugin plugin = getPlugin();
+        String pluginName = plugin.getName();
+
+        NBTTagCompound customData = nbtData.getCompound(pluginName);
+        String string = customData.getString(key);
+        return (string == null ? defaultValue : string);
+    }
+
+    @Override
+    public ItemStack removeCustomNBT(ItemStack item, String key) {
+        if(item == null || key == null || key.isEmpty()) return item;
+
+        net.minecraft.server.v1_13_R2.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
+        NBTTagCompound nbtData = nmsItem.hasTag() ? nmsItem.getTag() : new NBTTagCompound();
+        if(nbtData == null) nbtData = new NBTTagCompound();
+
+        JavaPlugin plugin = getPlugin();
+        String pluginName = plugin.getName();
+
+        NBTTagCompound customData = nbtData.getCompound(pluginName);
+        customData.remove(key);
+
+        if (customData.isEmpty()) nbtData.remove(pluginName);
+        else nbtData.set(pluginName, customData);
+
+        nmsItem.setTag(nbtData);
+        return CraftItemStack.asBukkitCopy(nmsItem);
+    }
+}
---
+++ nms/1_15_R1/src/main/java/com/SirBlobman/api/nms/HeadHandler_1_15_R1.java
@@ -0,0 +1,83 @@
+package com.SirBlobman.api.nms;
+
+import java.lang.reflect.Field;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.cryptomorin.xseries.XMaterial;
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import com.mojang.authlib.properties.PropertyMap;
+
+public class HeadHandler_1_15_R1 extends HeadHandler {
+    public HeadHandler_1_15_R1(JavaPlugin plugin) {
+        super(plugin);
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public ItemStack getPlayerHead(String username) {
+        OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(username);
+        return getPlayerHead(offlinePlayer);
+    }
+
+    @Override
+    public ItemStack getPlayerHead(OfflinePlayer player) {
+        ItemStack item = XMaterial.PLAYER_HEAD.parseItem();
+        if(item == null) return null;
+
+        SkullMeta meta = (SkullMeta) item.getItemMeta();
+        if(meta == null) return null;
+
+        meta.setOwningPlayer(player);
+        item.setItemMeta(meta);
+        return item;
+    }
+
+    @Override
+    public ItemStack getBase64Head(String base64) {
+        byte[] base64ByteArray = base64.getBytes();
+        UUID uuid = UUID.nameUUIDFromBytes(base64ByteArray);
+        return getBase64Head(base64, uuid);
+    }
+
+    @Override
+    public ItemStack getBase64Head(String base64, UUID customId) {
+        GameProfile gameProfile = new GameProfile(customId, "custom");
+        Property property = new Property("textures", base64);
+
+        PropertyMap properties = gameProfile.getProperties();
+        properties.put("textures", property);
+        return createGameProfileHead(gameProfile);
+    }
+
+    private ItemStack createGameProfileHead(GameProfile gameProfile) {
+        ItemStack item = XMaterial.PLAYER_HEAD.parseItem();
+        if(item == null) return null;
+
+        SkullMeta meta = (SkullMeta) item.getItemMeta();
+        if(meta == null) return null;
+
+        try {
+            Class<? extends SkullMeta> metaClass = meta.getClass();
+            Field profileField = metaClass.getDeclaredField("profile");
+            profileField.setAccessible(true);
+            profileField.set(meta, gameProfile);
+        } catch(Exception ex) {
+            JavaPlugin plugin = getPlugin();
+            Logger logger = plugin.getLogger();
+            logger.log(Level.WARNING, "Failed to create GameProfile head because of an error:", ex);
+            return null;
+        }
+
+        item.setItemMeta(meta);
+        return item;
+    }
+}
