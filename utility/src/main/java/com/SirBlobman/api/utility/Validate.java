3eb0521a2dfa87ce164c0d6d12a0a1b3bb48a549
SirBlobman <blobmansrealm@gmail.com>
2020-09-03 00:54:34 -0400

    2.0-SNAPSHOT: Nullable Fixes

--- utility/src/main/java/com/SirBlobman/api/utility/Validate.java
+++ utility/src/main/java/com/SirBlobman/api/utility/Validate.java
@@ -1,12 +1,14 @@
 package com.SirBlobman.api.utility;
 
+import org.jetbrains.annotations.Nullable;
+
 public final class Validate {
-    public static <O> O notNull(O value, String message) {
+    public static <O> O notNull(@Nullable O value, String message) {
         if(value == null) throw new IllegalArgumentException(message);
         return value;
     }
 
-    public static String notEmpty(String value, String message) {
+    public static String notEmpty(@Nullable String value, String message) {
         notNull(value, message);
         if(value.isEmpty()) throw new IllegalArgumentException(message);
         return value;
