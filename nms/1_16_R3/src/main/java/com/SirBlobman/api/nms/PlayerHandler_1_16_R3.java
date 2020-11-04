807a477b69046f498c0aa3663e719487162d1a21
SirBlobman <blobmansrealm@gmail.com>
2020-11-03 19:05:09 -0500

    1.0.5: Added support for 1.16.4 NMS 1_16_R3

---
+++ nms/1_16_R3/src/main/java/com/SirBlobman/api/nms/PlayerHandler_1_16_R3.java
@@ -0,0 +1,84 @@
+package com.SirBlobman.api.nms;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Player.Spigot;
+import org.bukkit.plugin.java.JavaPlugin;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
+
+import net.minecraft.server.v1_16_R3.EntityPlayer;
+import net.minecraft.server.v1_16_R3.IChatBaseComponent;
+import net.minecraft.server.v1_16_R3.IChatBaseComponent.ChatSerializer;
+import net.minecraft.server.v1_16_R3.PacketDataSerializer;
+import net.minecraft.server.v1_16_R3.PacketPlayOutPlayerListHeaderFooter;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer;
+
+import io.netty.buffer.Unpooled;
+
+public class PlayerHandler_1_16_R3 extends PlayerHandler {
+    public PlayerHandler_1_16_R3(JavaPlugin plugin) {
+        super(plugin);
+    }
+
+    @Override
+    public void sendActionBar(Player player, String message) {
+        BaseComponent[] baseComponents = TextComponent.fromLegacyText(message);
+        Spigot spigot = player.spigot();
+        spigot.sendMessage(ChatMessageType.ACTION_BAR, baseComponents);
+    }
+
+    @Override
+    public void sendTabInfo(Player player, String header, String footer) {
+        String headerJSON = asJSON(header);
+        String footerJSON = asJSON(footer);
+        IChatBaseComponent headerComponent = ChatSerializer.a(headerJSON);
+        IChatBaseComponent footerComponent = ChatSerializer.a(footerJSON);
+
+        CraftPlayer craftPlayer = (CraftPlayer) player;
+        EntityPlayer nmsPlayer = craftPlayer.getHandle();
+        try {
+            PacketDataSerializer packetData = new PacketDataSerializer(Unpooled.buffer());
+            packetData.a(headerComponent);
+            packetData.a(footerComponent);
+
+            PacketPlayOutPlayerListHeaderFooter packet = new PacketPlayOutPlayerListHeaderFooter();
+            packet.a(packetData);
+            nmsPlayer.playerConnection.sendPacket(packet);
+        } catch(IOException ex) {
+            JavaPlugin plugin = getPlugin();
+            Logger logger = plugin.getLogger();
+            logger.log(Level.WARNING, "An error occurred while sending a tab packet:", ex);
+        }
+    }
+
+    @Override
+    public void forceRespawn(Player player) {
+        Spigot spigot = player.spigot();
+        spigot.respawn();
+    }
+
+    @Override
+    public double getAbsorptionHearts(Player player) {
+        CraftPlayer craftPlayer = (CraftPlayer) player;
+        EntityPlayer nmsPlayer = craftPlayer.getHandle();
+        return nmsPlayer.getAbsorptionHearts();
+    }
+
+    @Override
+    public void setAbsorptionHearts(Player player, double hearts) {
+        CraftPlayer craftPlayer = (CraftPlayer) player;
+        EntityPlayer nmsPlayer = craftPlayer.getHandle();
+        nmsPlayer.setAbsorptionHearts((float) hearts);
+    }
+
+    @Override
+    public void sendCooldownPacket(Player player, Material material, int ticksLeft) {
+        player.setCooldown(material, ticksLeft);
+    }
+}
