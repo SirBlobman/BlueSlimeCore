f395747bd62481f8ddd05a0c27075e0190c69645
SirBlobman <blobmansrealm@gmail.com>
2020-12-29 19:48:25 -0500

    2.0.0-SNAPSHOT: 1.0.10: Added loadItemStack to AdvancedAbstractMenu

--- menu/src/main/java/com/SirBlobman/api/menu/AdvancedAbstractMenu.java
+++ menu/src/main/java/com/SirBlobman/api/menu/AdvancedAbstractMenu.java
@@ -1,9 +1,17 @@
 package com.SirBlobman.api.menu;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+import com.SirBlobman.api.item.ItemBuilder;
+import com.SirBlobman.api.item.SkullBuilder;
+import com.SirBlobman.api.nms.HeadHandler;
 import com.SirBlobman.api.utility.MessageUtility;
 import com.SirBlobman.api.utility.Validate;
 
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -15,10 +23,13 @@ import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.InventoryView;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitScheduler;
+
+import com.cryptomorin.xseries.XMaterial;
 
 public abstract class AdvancedAbstractMenu<Plugin extends JavaPlugin> extends BukkitRunnable implements InventoryHolder, Listener {
     private final Plugin plugin;
@@ -111,6 +122,69 @@ public abstract class AdvancedAbstractMenu<Plugin extends JavaPlugin> extends Bu
     private void internalClose() {
         HandlerList.unregisterAll(this);
         cancel();
+    }
+
+    public HeadHandler getHeadHandler() {
+        return null;
+    }
+
+    protected final ItemStack loadItemStack(ConfigurationSection config, String path) {
+        if(config.isItemStack(path)) return config.getItemStack(path);
+        ConfigurationSection section = config.getConfigurationSection(path);
+        return loadItemStack(section);
+    }
+
+    private ItemStack loadItemStack(ConfigurationSection section) {
+        if(section == null) return null;
+
+        String materialName = section.getString("material");
+        Optional<XMaterial> xMaterial = XMaterial.matchXMaterial(materialName);
+        if(!xMaterial.isPresent()) {
+            Logger logger = this.plugin.getLogger();
+            logger.warning("Unknown material name '" + materialName + "'.");
+            return null;
+        }
+
+        XMaterial realMaterial = xMaterial.get();
+        ItemBuilder builder = new ItemBuilder(realMaterial);
+        builder = checkSkull(builder, realMaterial, section);
+
+        int amount = section.getInt("quantity");
+        builder.withAmount(amount);
+
+        int damage = section.getInt("damage");
+        builder.withDamage(damage);
+
+        Integer model = section.isSet("model") ? section.getInt("model") : null;
+        builder.withModel(model);
+
+        String displayName = section.getString("display-name");
+        if(displayName != null) {
+            String displayNameColored = MessageUtility.color(displayName);
+            builder.withName(displayNameColored);
+        }
+
+        List<String> loreList = section.getStringList("lore");
+        if(!loreList.isEmpty()) {
+            List<String> loreListColored = MessageUtility.colorList(loreList);
+            builder.withLore(loreListColored);
+        }
+
+        if(section.getBoolean("glowing")) builder.withGlowing();
+        return builder.build();
+    }
+
+    private ItemBuilder checkSkull(ItemBuilder builder, XMaterial xMaterial, ConfigurationSection section) {
+        HeadHandler headHandler = getHeadHandler();
+        if(xMaterial != XMaterial.PLAYER_HEAD || headHandler == null) return builder;
+
+        String texture = section.getString("texture");
+        if(texture != null) return SkullBuilder.withTexture(headHandler, texture);
+
+        String username = section.getString("skull-owner");
+        if(username != null) return SkullBuilder.withOwner(headHandler, username);
+
+        return builder;
     }
 
     public abstract Inventory getInventory();
