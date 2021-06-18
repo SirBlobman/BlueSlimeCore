d7b15b61d4162338f2c7bb325a670dd6e24a221e
SirBlobman <blobmansrealm@gmail.com>
2021-06-17 20:10:41 -0400

    2.2-SNAPSHOT: 2.2.2-Dev: Added next/previous page button classes to API

---
+++ menu/src/main/java/com/github/sirblobman/api/menu/button/NextPageButton.java
@@ -0,0 +1,19 @@
+package com.github.sirblobman.api.menu.button;
+
+import org.bukkit.entity.Player;
+
+import com.github.sirblobman.api.menu.AbstractPagedMenu;
+import com.github.sirblobman.api.utility.Validate;
+
+public final class NextPageButton extends QuickButton {
+    private final AbstractPagedMenu pagedMenu;
+
+    public NextPageButton(AbstractPagedMenu pagedMenu) {
+        this.pagedMenu = Validate.notNull(pagedMenu, "pagedMenu must not be null!");
+    }
+
+    @Override
+    public void onLeftClick(Player player, boolean shift) {
+        this.pagedMenu.openNextPage();
+    }
+}
---
+++ menu/src/main/java/com/github/sirblobman/api/menu/button/PreviousPageButton.java
@@ -0,0 +1,19 @@
+package com.github.sirblobman.api.menu.button;
+
+import org.bukkit.entity.Player;
+
+import com.github.sirblobman.api.menu.AbstractPagedMenu;
+import com.github.sirblobman.api.utility.Validate;
+
+public final class PreviousPageButton extends QuickButton {
+    private final AbstractPagedMenu pagedMenu;
+
+    public PreviousPageButton(AbstractPagedMenu pagedMenu) {
+        this.pagedMenu = Validate.notNull(pagedMenu, "pagedMenu must not be null!");
+    }
+
+    @Override
+    public void onLeftClick(Player player, boolean shift) {
+        this.pagedMenu.openPreviousPage();
+    }
+}
