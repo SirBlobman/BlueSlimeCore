52d093850253d9c0819b5ed87e744b4464e7c133
SirBlobman <blobmansrealm@gmail.com>
2021-06-22 07:48:28 -0400

    Begin working on Factions handler and helper.

---
+++ factions/abstract/src/main/java/com/github/sirblobman/api/factions/FactionsHandler.java
@@ -0,0 +1,74 @@
+package com.github.sirblobman.api.factions;
+
+import java.util.Set;
+import java.util.UUID;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Entity;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.github.sirblobman.api.utility.Validate;
+
+public abstract class FactionsHandler {
+    private final JavaPlugin plugin;
+
+    public FactionsHandler(JavaPlugin plugin) {
+        this.plugin = Validate.notNull(plugin, "plugin must not be null!");
+    }
+
+    public Object getFactionAt(Entity entity) {
+        Location location = entity.getLocation();
+        return getFactionAt(location);
+    }
+
+    public abstract Object getFactionAt(Location location);
+    public abstract String getFactionNameAt(Location location);
+
+    public abstract boolean isSafeZone(Location location);
+    public abstract boolean isWarZone(Location location);
+    public abstract boolean isWilderness(Location location);
+
+    public abstract boolean hasFaction(OfflinePlayer player);
+    public abstract Object getFactionFor(OfflinePlayer player);
+    public abstract Object getFactionNameFor(OfflinePlayer player);
+
+    /**
+     * @param player1 The first player
+     * @param player2 The second player
+     * @return {@code true} if player2 is the same as player1.
+     * {@code false} if either player is not in a faction.
+     * {@code true} if player2 has an ally relationship with player1.
+     * {@code true} if player2 is in the same faction as player1.
+     * {@code false} otherwise
+     */
+    public abstract boolean isMemberOrAlly(OfflinePlayer player1, OfflinePlayer player2);
+
+    /**
+     * @param player1 The first player
+     * @param player2 The second player
+     * @return {@code false} if player2 is the same as player1.
+     * {@code false} if either player is not in a faction.
+     * {@code true} if player2 has an enemy relationship with player1.
+     * {@code false} otherwise
+     */
+    public abstract boolean isEnemy(OfflinePlayer player1, OfflinePlayer player2);
+
+    /**
+     * @param player The player to check
+     * @return {@code true} if the player has admin bypass mode enabled, {@code false} otherwise.
+     */
+    public abstract boolean hasBypass(OfflinePlayer player);
+
+    public abstract boolean isInEnemyLand(OfflinePlayer player, Location location);
+    public abstract boolean isInOwnFaction(OfflinePlayer player, Location location);
+    public abstract boolean isLeader(OfflinePlayer player, Location location);
+    public abstract boolean canBuild(OfflinePlayer player, Location location);
+
+    public abstract ChatColor getRelationChatColor(OfflinePlayer viewer, OfflinePlayer player);
+    public abstract String getRolePrefix(OfflinePlayer player);
+
+    public abstract Set<UUID> getMembersForFactionAt(Location location);
+    public abstract Set<UUID> getMembersForFactionOf(OfflinePlayer player);
+}
---
+++ factions/handler/src/main/java/com/github/sirblobman/api/factions/FactionsHelper.java
@@ -0,0 +1,90 @@
+package com.github.sirblobman.api.factions;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.github.sirblobman.api.utility.Validate;
+
+public final class FactionsHelper {
+    private final JavaPlugin plugin;
+    private FactionsHandler factionsHandler;
+
+    public FactionsHelper(JavaPlugin plugin) {
+        this.plugin = Validate.notNull(plugin, "plugin must not be null!");
+        this.factionsHandler = null;
+    }
+
+    public JavaPlugin getPlugin() {
+        return this.plugin;
+    }
+
+    public FactionsHandler getFactionsHandler() {
+        if(this.factionsHandler != null) {
+            return this.factionsHandler;
+        }
+
+        try {
+            PluginManager manager = Bukkit.getPluginManager();
+            if(manager.isPluginEnabled("FactionsX")) {
+                printHookInfo("FactionsX", "FactionsX");
+                this.factionsHandler = new FactionsHandler_X(this.plugin);
+                return this.factionsHandler;
+            }
+
+            if(manager.isPluginEnabled("LegacyFactions")) {
+                printHookInfo("LegacyFactions", "Legacy Factions");
+                this.factionsHandler = new FactionsHandler_Legacy(this.plugin);
+                return this.factionsHandler;
+            }
+
+            Plugin plugin = getPlugin("Factions");
+            if(plugin == null) throw new IllegalStateException("Could not find a plugin named 'Factions'.");
+
+            PluginDescriptionFile description = plugin.getDescription();
+            String pluginVersion = description.getVersion();
+
+            if(pluginVersion.startsWith("1.6.9.5-U0.2")) {
+                printHookInfo("Factions", "Factions UUID Legacy");
+                this.factionsHandler = new FactionsHandler_UUID_Legacy(this.plugin);
+                return this.factionsHandler;
+            }
+
+            if(pluginVersion.startsWith("1.6.9.5-U0.5")) {
+                printHookInfo("Factions", "Factions UUID");
+                this.factionsHandler = new FactionsHandler_UUID(this.plugin);
+                return this.factionsHandler;
+            }
+
+            printHookInfo("Factions", "MassiveCore Factions");
+            this.factionsHandler = new FactionsHandler_Massive(this.plugin);
+            return this.factionsHandler;
+        } catch(Exception ex) {
+            Logger logger = getPlugin().getLogger();
+            logger.log(Level.WARNING,"Could not find a Factions plugin to hook into because an error occurred:",
+                    ex);
+            return null;
+        }
+    }
+
+    private Plugin getPlugin(String pluginName) {
+        PluginManager manager = Bukkit.getPluginManager();
+        return manager.getPlugin(pluginName);
+    }
+
+    private void printHookInfo(String pluginName, String hookName) {
+        Plugin plugin = getPlugin(pluginName);
+        if(plugin == null) return;
+
+        PluginDescriptionFile description = plugin.getDescription();
+        String pluginVersion = description.getVersion();
+
+        Logger logger = this.plugin.getLogger();
+        logger.info("Successfully hooked into '" + hookName + " " + pluginVersion + " '.");
+    }
+}
