f7cce22c382ce0534b2e0caa435b43efd6d519d9
SirBlobman <blobmansrealm@gmail.com>
2022-09-03 21:35:46 -0400

    2.5.5-Dev: Add ComplexReplacer.

---
+++ language/src/main/java/com/github/sirblobman/api/language/ComplexReplacer.java
@@ -0,0 +1,29 @@
+package com.github.sirblobman.api.language;
+
+import com.github.sirblobman.api.utility.Validate;
+
+public final class ComplexReplacer implements Replacer {
+    private final Replacer[] replacerArray;
+
+    public ComplexReplacer(Replacer... replacerArray) {
+        this.replacerArray = Validate.notNull(replacerArray, "replacerArray must not be null!");
+    }
+
+    public Replacer[] getReplacerArray() {
+        return this.replacerArray;
+    }
+
+    @Override
+    public String replace(String string) {
+        Replacer[] replacerArray = getReplacerArray();
+        for (Replacer replacer : replacerArray) {
+            if(replacer == null) {
+                continue;
+            }
+
+            string = replacer.replace(string);
+        }
+
+        return string;
+    }
+}
