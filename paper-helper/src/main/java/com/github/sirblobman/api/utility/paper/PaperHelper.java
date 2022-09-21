e8d92898ea22675d0b8b777bd8618b6b6fec76eb
SirBlobman <blobmansrealm@gmail.com>
2022-09-21 18:53:21 -0400

    2.6-SNAPSHOT: add getDisplayName and getLore to paper helper.

--- paper-helper/src/main/java/com/github/sirblobman/api/utility/paper/PaperHelper.java
+++ paper-helper/src/main/java/com/github/sirblobman/api/utility/paper/PaperHelper.java
@@ -9,12 +9,61 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 import com.github.sirblobman.api.adventure.adventure.text.Component;
 
+import org.jetbrains.annotations.Nullable;
+
+import static com.github.sirblobman.api.utility.paper.ComponentConverter.normalToShaded;
 import static com.github.sirblobman.api.utility.paper.ComponentConverter.shadedToNormal;
 
 public final class PaperHelper {
     public static void setDeathMessage(Component message, PlayerDeathEvent e) {
         net.kyori.adventure.text.Component paperMessage = shadedToNormal(message);
         e.deathMessage(paperMessage);
+    }
+
+    @Nullable
+    public static Component getDisplayName(ItemStack item) {
+        if (item == null) {
+            return null;
+        }
+
+        ItemMeta itemMeta = item.getItemMeta();
+        if (itemMeta == null) {
+            return null;
+        }
+
+        if (itemMeta.hasDisplayName()) {
+            net.kyori.adventure.text.Component paperName = itemMeta.displayName();
+            if (paperName == null) {
+                return null;
+            }
+
+            return normalToShaded(paperName);
+        }
+
+        return null;
+    }
+
+    @Nullable
+    public static List<Component> getLore(ItemStack item) {
+        if (item == null) {
+            return null;
+        }
+
+        ItemMeta itemMeta = item.getItemMeta();
+        if (itemMeta == null) {
+            return null;
+        }
+
+        if (itemMeta.hasLore()) {
+            List<net.kyori.adventure.text.Component> paperLore = itemMeta.lore();
+            if (paperLore == null) {
+                return null;
+            }
+
+            return paperLore.parallelStream().map(ComponentConverter::normalToShaded).collect(Collectors.toList());
+        }
+
+        return null;
     }
 
     public static void setDisplayName(ItemStack item, Component name) {
