493887bb4e626b7725181058bc835ad8fc70b0f5
SirBlobman <blobmansrealm@gmail.com>
2026-05-15 18:24:26 -0400

    Update XSeries to v13.7.0

--- shaded/build.gradle.kts
+++ shaded/build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 
 dependencies {
     // XSeries
-    implementation("com.github.cryptomorin:XSeries:13.6.0")
+    implementation("com.github.cryptomorin:XSeries:13.7.0")
 
     // Adventure
     implementation("net.kyori:adventure-platform-bukkit:4.4.1")
