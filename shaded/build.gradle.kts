d1b65fd71e4a62d87d56629686a0352abdb456a9
SirBlobman <blobmansrealm@gmail.com>
2026-07-20 18:38:44 -0400

    Remove unnecessary version setting in shaded project.

--- shaded/build.gradle.kts
+++ shaded/build.gradle.kts
@@ -5,12 +5,6 @@ plugins {
     id("com.gradleup.shadow") version "9.3.1"
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
-    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
-}
-
 repositories {
     maven("https://nexus.sirblobman.xyz/public/")
 }
