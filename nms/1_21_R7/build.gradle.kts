86d0c3ba4d9e9ac548dacdfd5cc0071bea2040a6
SirBlobman <blobmansrealm@gmail.com>
2026-08-23 04:08:58 -0400

    Fix build errors. (Attempt 02)

--- nms/1_21_R7/build.gradle.kts
+++ nms/1_21_R7/build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     compileOnly(project(":nms:modern-nbt"))
 
     // SpigotMC Mojang Mapped NMS
-    compileOnly("org.spigotmc:spigot:1.21.11-R0.2-SNAPSHOT:remapped-mojang")
+    compileOnly("org.spigotmc:spigot:1.21.11-R0.1-SNAPSHOT:remapped-mojang")
 }
 
 tasks {
